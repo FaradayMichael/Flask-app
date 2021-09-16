4a12ce382a58690017bce3acb22792a7edc6df46
FaradayMichael <faraday62@mail.ru>
2021-09-16 15:52:24 +0300

    ¿

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,14 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import LoginForm
+import psycopg2
+from sqlalchemy import create_engine
 
+database = "FlaskApp"
+user = "postgres"
+password = "root"
+host = "127.0.0.1"
+port = "5432"
 
 @app.route('/')
 @app.route('/index')
@@ -9,7 +16,16 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        connection = psycopg2.connect(user="postgres",
+                                      password="root",
+                                      host="localhost",
+                                      port="5432",
+                                      database="FlaskApp")
+        cur = connection.cursor()
+        cur.execute("select * from public.\"USERS\"")
+        print(cur.fetchone())
     return render_template("login.html", form=form)
