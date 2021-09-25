0cf7e434847ff597f1dffb1a5f4b5d784f2f34df
FaradayMichael <faraday62@mail.ru>
2021-09-26 00:32:06 +0300

    ¿

--- app/forms.py
+++ app/forms.py
@@ -7,3 +7,7 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
+
+
+class RegisterForm(LoginForm):
+    submit = SubmitField('Register')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app
-from flask import render_template
-from app.forms import LoginForm
-
+from flask import render_template, flash, redirect
+from app.forms import LoginForm, RegisterForm
+from app.entities import Users
+from app import db
 
 
 @app.route('/')
@@ -18,3 +19,19 @@ def login():
         # if con.find_user(form.username.data, form.password.data):
         #     pass  # TODO action after login
     return render_template("login.html", form=form)
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if not Users.query.filter(Users.username==form.username.data).first():
+            user = Users(username=form.username.data, passw=form.password.data, adm=False)
+            db.session.add(user)
+            db.session.commit()
+            return redirect('/index')
+        else:
+            print("no")
+            flash("No")
+            redirect("/index")
+    return render_template("register.html", form=form)
