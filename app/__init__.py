6a33815fbe9b18098b6f9a17fe1d4a6770e1aa47
FaradayMichael <faraday62@mail.ru>
2021-09-26 00:33:35 +0300

    ¿

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from app.connection import Connection
 
 app = Flask(__name__)
 app.config.from_object(Config)
